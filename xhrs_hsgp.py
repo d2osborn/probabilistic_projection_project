535c77cb8c397de680920cf60dfd7ad6318b71f5
Diego Osborn <diegoisaacosborn@gmail.com>
2026-03-12 23:13:16 -0700

    more comment updates

--- xhrs_hsgp.py
+++ xhrs_hsgp.py
@@ -69,7 +69,7 @@ def hsgp_model_run():
 
         def model(self, X: jax.Array, y: jax.Array | None = None):
             amplitude = numpyro.sample("amplitude", dist.LogNormal(0, 1)) ## A ~ LogNormal(0, 1) --> LogNormal(0, 1) = A > 0
-            length = numpyro.sample("lengthscale", dist.Exponential(jnp.ones(2))) ## l ~ Exponential([1, 1]) --> one lengthscale for EV and one for LA
+            length = numpyro.sample("lengthscale", dist.Exponential(jnp.ones(2))) ## l_1, l_2 ~ Exp(1) --> one lengthscale for EV and one for LA
             f_centered = hsgp_matern(X, alpha=amplitude, length=length, ell=self.L, m=self.m, nu=2.5) ## nu = 2.5 makes it matern 5/2 kernel
             f = numpyro.deterministic("f_star", f_centered + log_odds_baseline) ## we're setting the m(x) at -3 (in log odds) ≈ 0.0474 HR%
             site = "y" if y is not None else "y_test"
